c8f5cc083aaff9bd69972fd6eb7d781bfe2c5cf9
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-14 11:25:45 +0530

    add: `__str__` on models

--- csv_handler/models.py
+++ csv_handler/models.py
@@ -15,9 +15,15 @@ class Base(models.Model):
 class CSVFile(Base):
     name = models.CharField(max_length=155)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class CSVRow(Base):
     name = models.CharField(max_length=155)
     age = models.IntegerField(validators=[MinValueValidator(18)])
     sex = models.CharField(choices=SEX_CHOICES, max_length=1)
     file = models.ForeignKey(to=CSVFile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'[{self.id}] {self.name} ({self.age}) ({self.sex})'
