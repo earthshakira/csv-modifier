c2b7168e26d5c5ac0b9651e35cd7a3b80b919b7a
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-14 11:32:58 +0530

    mod: add appropriate fields on ModelSerializers

--- csv_handler/serializers.py
+++ csv_handler/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.relations import PrimaryKeyRelatedField
 
 from csv_handler.models import CSVFile, CSVRow
 
@@ -6,12 +7,14 @@ from csv_handler.models import CSVFile, CSVRow
 class FileModelSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = CSVFile
-        fields = ['name']
+        fields = ['name', 'created_at', 'updated_at']
         read_only_fields = ('created_at', 'updated_at')
 
 
 class RowModelSerializer(serializers.HyperlinkedModelSerializer):
+    file = PrimaryKeyRelatedField(queryset=CSVFile.objects.all())
+
     class Meta:
         model = CSVRow
-        fields = ['name', 'age', 'sex']
+        fields = ['id', 'name', 'age', 'sex', 'file', 'created_at', 'updated_at']
         read_only_fields = ('created_at', 'updated_at')
