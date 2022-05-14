2671134f04c75951947e8a110239f376bad060a7
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-14 11:57:42 +0530

    add: filtering on rows view set

--- csv_handler/views.py
+++ csv_handler/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions, generics
 
 from csv_handler.models import CSVFile, CSVRow
 from csv_handler.serializers import FileModelSerializer, RowModelSerializer
@@ -17,6 +18,8 @@ class RowViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows Rows to be viewed or edited.
     """
-    queryset = CSVRow.objects.all()
+    queryset = CSVRow.objects.all().order_by('id')
     serializer_class = RowModelSerializer
     permission_classes = [permissions.AllowAny]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['file', 'name', 'age', 'sex']
