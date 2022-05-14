1c6d397f3a2982056a72abf72c52b1671ac4645d
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-14 11:18:11 +0530

    fix: missing routes urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 import csv_handler.views
@@ -23,5 +23,6 @@ router = routers.DefaultRouter()
 router.register(r'rows', csv_handler.views.RowViewSet)
 router.register(r'files', csv_handler.views.FileViewSet)
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
