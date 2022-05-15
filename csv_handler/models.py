176db1ad6408b9ed1a28d20a1f7ce2ff8cc6d68e
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-15 21:19:03 +0530

    remove: parent base model

--- csv_handler/models.py
+++ csv_handler/models.py
@@ -7,23 +7,22 @@ SEX_CHOICES = (
 )
 
 
-class Base(models.Model):
+class CSVFile(models.Model):
+    name = models.CharField(max_length=155, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class CSVFile(Base):
-    name = models.CharField(max_length=155, unique=True)
 
     def __str__(self):
         return f'{self.name}'
 
 
-class CSVRow(Base):
+class CSVRow(models.Model):
     name = models.CharField(max_length=155)
     age = models.IntegerField(validators=[MinValueValidator(18)])
     sex = models.CharField(choices=SEX_CHOICES, max_length=1)
     file = models.ForeignKey(to=CSVFile, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'[{self.id}] {self.name} ({self.age}) ({self.sex})'
