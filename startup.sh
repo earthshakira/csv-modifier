6c7703e283e220ac9b0e2dfd4acaf72d649399ff
Shubham Arawkar <arawkar.shubham08@gmail.com>
2022-05-16 18:12:02 +0530

    fix: port number

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@ mkdir staticfiles
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py collectstatic
-gunicorn core.wsgi -b 0.0.0.0:$(PORT)--log-file -
+gunicorn core.wsgi -b 0.0.0.0:$PORT --log-file -
